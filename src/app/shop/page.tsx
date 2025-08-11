9ef3baf2998aa1d55134b9dc1df4c67870026775
Mahmoud saeed <Mahmoudsaeed0112074@gamil.com>
2025-08-11 17:53:18 +0300

    feat: add shop page

---
+++ src/app/shop/page.tsx
@@ -0,0 +1,11 @@
+import ShopSection from "@/components/Shop/ProductsSection";
+import BreadcrumbHeader from "@/components/ui/BreadcrumbHeader";
+import React from "react";
+export default function Page() {
+  return (
+    <>
+      <BreadcrumbHeader title="shope" />
+      <ShopSection />
+    </>
+  );
+}
